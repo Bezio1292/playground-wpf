68377a9b13deda46722851ee6c51b2232ca11f9d
Bartosz Kowalski <bezio1292@gmail.com>
2023-07-28 16:58:15 +0200

    Adds three new custom dependency properties.

--- Dependency Properties/UserControls/DemoUserControl.xaml.cs
+++ Dependency Properties/UserControls/DemoUserControl.xaml.cs
@@ -1,28 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Dependency_Properties.UserControls
 {
-    /// <summary>
-    /// Interaction logic for DemoUserControl.xaml
-    /// </summary>
     public partial class DemoUserControl : UserControl
     {
         public DemoUserControl()
         {
             InitializeComponent();
         }
+
+        // propdp + TAB + TAB
+        // This shortcut creates default structure for custom dependency property
+        public int Awesomeness
+        {
+            get { return (int)GetValue(AwesomenessProperty); }
+            set { SetValue(AwesomenessProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Awesomeness.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AwesomenessProperty =
+            DependencyProperty.Register("Awesomeness", typeof(int), typeof(DemoUserControl), new PropertyMetadata(0)); // new PropertyMetadata(0) == new PropertyMetadata(defalutValue: 0)
+
+
+
+
+        public bool AwesomenessOverValue
+        {
+            get { return (bool)GetValue(AwesomenessOverValueProperty); }
+            set { SetValue(AwesomenessOverValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for AwesomenessOverValue.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AwesomenessOverValueProperty =
+            DependencyProperty.Register("AwesomenessOverValue", typeof(bool), typeof(DemoUserControl), new PropertyMetadata(false));
+
+
+
+
+        public string Postfix
+        {
+            get { return (string)GetValue(PostfixProperty); }
+            set { SetValue(PostfixProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Postfix.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PostfixProperty =
+            DependencyProperty.Register("Postfix", typeof(string), typeof(DemoUserControl), new PropertyMetadata(defaultValue: string.Empty, new PropertyChangedCallback(OnPostfixChanged)));
+
+        private static void OnPostfixChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DemoUserControl? MyUCControl = d as DemoUserControl;
+            MyUCControl?.OnPostfixChanged(e);
+        }
+
+        private void OnPostfixChanged(DependencyPropertyChangedEventArgs e)
+        {
+            customTextBlock.Text += $" {e.NewValue}";
+        }
+
     }
 }

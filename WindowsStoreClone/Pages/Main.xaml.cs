b594c9944835ab515183f61312345a9d280eaa11
Bartosz Kowalski <bezio1292@gmail.com>
2023-08-02 17:18:50 +0200

    83. Creating Animations in Code Behind.

--- WindowsStoreClone/Pages/Main.xaml.cs
+++ WindowsStoreClone/Pages/Main.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -24,5 +25,23 @@ namespace WindowsStoreClone.Pages
         {
             InitializeComponent();
         }
+
+        // c# code has LOWER priority than XAML code !!!!
+        
+        private void MainScrollViewer_Loaded(object sender, RoutedEventArgs e)
+        {
+            UIElement element= sender as UIElement;
+            element.Opacity= 0;
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = 0,
+                To = 1,
+                Duration = new Duration(new TimeSpan(0, 0, 1))
+            };
+            // Commented because c# code has lower priority than XAML code so it wouldn't be executed anyway.
+            //element.BeginAnimation(UIElement.OpacityProperty, animation);
+
+        }
+        
     }
 }

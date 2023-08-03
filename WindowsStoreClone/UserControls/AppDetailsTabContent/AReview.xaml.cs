b38a7939bf0a8318c1fad63d7be2ffe98ced7a14
Bartosz Kowalski <bezio1292@gmail.com>
2023-08-03 18:24:12 +0200

    93. Creating the Reviews Tab

---
+++ WindowsStoreClone/UserControls/AppDetailsTabContent/AReview.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MiscUtil;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace WindowsStoreClone.UserControls.AppDetailsTabContent
+{
+    /// <summary>
+    /// Interaction logic for AReview.xaml
+    /// </summary>
+    public partial class AReview : UserControl
+    {
+        List<string> Names;
+        List<string> Titles;
+        public AReview()
+        {
+            InitializeComponent();
+            Names = new List<string>() { "Vic", "Mike", "Zoltan", "Daniel", "Maria" };
+            Titles = new List<string>() { "This app is awful!", "This app is bad!", "It's ok", "Nice app :D", "This app is a masterpiece" };
+
+            string reviewerName = Names[StaticRandom.Next(Names.Count)];
+            ReviewerNameLabel.Content = reviewerName;
+            AvatarLabel.Content = reviewerName[0];
+            NumOfStarsLabel.Content = GetRandomNumOfStars();
+            ReviewTitle.Content = Titles[NumOfStarsLabel.Content.ToString().Length-1];
+        }
+
+        private string GetRandomNumOfStars()
+        {
+            string content = string.Empty;
+            for (int i = 0; i < StaticRandom.Next(1, 5); ++i)
+            {
+                content += "★";
+            }
+            return content;
+        }
+    }
+}

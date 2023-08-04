5bef4aa5b40cf6e4229716670e4468dd308de779
Bartosz Kowalski <bezio1292@gmail.com>
2023-08-04 14:39:05 +0200

    101. Preparing the HamburgerMenuAppList

--- WindowsStoreClone/UserControls/HamburgerMenuViews/HamburgerMenuApp.xaml.cs
+++ WindowsStoreClone/UserControls/HamburgerMenuViews/HamburgerMenuApp.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
+﻿using MiscUtil;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WindowsStoreClone.UserControls.HamburgerMenuViews
 {
@@ -20,9 +14,28 @@ namespace WindowsStoreClone.UserControls.HamburgerMenuViews
     /// </summary>
     public partial class HamburgerMenuApp : UserControl
     {
+        private List<string> AppNames;
+        private List<string> AppTypes;
+        public string AppName { get; private set; }
+        public DateTime Purchased {get;private set;}
+        public string Type { get; private set; }
+
         public HamburgerMenuApp()
         {
             InitializeComponent();
+            AppTypes = new List<string>() { "App", "Game", "Movie", "Avatar" };
+
+            List<string> filepaths = Directory.GetFiles(Environment.CurrentDirectory + @"\..\..\Images\MiniIcons", "*.png").ToList<string>();
+            FileInfo myRandomFile = new FileInfo(filepaths[StaticRandom.Next(filepaths.Count)]);
+
+            Uri appImageSourceUri = new Uri(myRandomFile.FullName, UriKind.RelativeOrAbsolute);
+            AppName = (new CultureInfo("en-US", false).TextInfo).ToTitleCase(myRandomFile.Name.Split('-').Last().Split('.').First());
+            AppImage.Source = new BitmapImage(appImageSourceUri);
+            Type = AppTypes[StaticRandom.Next(AppTypes.Count)];
+            Purchased = new DateTime(2020, 1, StaticRandom.Next(1, DateTime.Now.Day + 1));
+            PurchasedLabel.Content = "Purchased " + Purchased.ToString("d");
+            AppNameLabel.Content = AppName;
+            
         }
     }
 }
---
+++ WindowsStoreClone/UserControls/HamburgerMenuViews/HamburgerMenuAppList.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace WindowsStoreClone.UserControls.HamburgerMenuViews
+{
+    /// <summary>
+    /// Interaction logic for HamburgerMenuAppList.xaml
+    /// </summary>
+    public partial class HamburgerMenuAppList : UserControl
+    {
+
+        private List<HamburgerMenuApp> AllApps;
+        private List<HamburgerMenuApp> AppsOnFilter;
+
+        public HamburgerMenuAppList()
+        {
+            InitializeComponent();
+            AllApps = new List<HamburgerMenuApp>();
+            AppsOnFilter = new List<HamburgerMenuApp>();
+            for (int i = 0; i < 50; ++i)
+                AddNewHamApp();
+        }
+
+
+        private void AddNewHamApp()
+        {
+            HamburgerMenuApp app = new HamburgerMenuApp();
+            MainStackPanel.Children.Add(app);
+            AllApps.Add(app);
+        }
+
+        public void FilterByType(string inFilter)
+        {
+            AppsOnFilter = AllApps.Where(p => p.Type == inFilter).ToList<HamburgerMenuApp>();
+            MainStackPanel.Children.Clear();
+            foreach (HamburgerMenuApp item in AppsOnFilter)
+            {
+                MainStackPanel.Children.Add(item);
+            }
+        }
+
+        public void AddAll()
+        {
+            MainStackPanel.Children.Clear();
+            foreach (HamburgerMenuApp item in AllApps)
+            {
+                MainStackPanel.Children.Add(item);
+            }
+        }
+
+        public void SortByName()
+        {
+            List<HamburgerMenuApp> AllAppsSorted = AllApps.OrderBy(p => p.AppName).ToList();
+            foreach(HamburgerMenuApp item in AllAppsSorted)
+            {
+                MainStackPanel.Children.Add(item);
+            }
+        }
+        public void SortByDate()
+        {
+            List<HamburgerMenuApp> AllAppsSorted = AllApps.OrderBy(p => p.Purchased).ToList();
+            foreach (HamburgerMenuApp item in AllAppsSorted)
+            {
+                MainStackPanel.Children.Add(item);
+            }
+        }
+    }
+}

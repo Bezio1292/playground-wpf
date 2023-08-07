32d881a624d8ccbae71f8dbba0c6b84ad7af16ea
Bartosz Kowalski <bezio1292@gmail.com>
2023-08-07 19:26:09 +0200

    Add Command.

---
+++ Reservoom/Commands/MakeReservationCommand.cs
@@ -0,0 +1,58 @@
+﻿using Reservoom.Exceptions;
+using Reservoom.Models;
+using Reservoom.ViewModels;
+using System.Windows;
+
+namespace Reservoom.Commands
+{
+    class MakeReservationCommand : CommandBase
+    {
+        private readonly MakeReservationViewModel _makeReservationViewModel;
+        private readonly Hotel _hotel;
+
+        public MakeReservationCommand(ViewModels.MakeReservationViewModel makeReservationViewModel, Hotel hotel)
+        {
+            _makeReservationViewModel = makeReservationViewModel;
+            _hotel = hotel;
+
+            _makeReservationViewModel.PropertyChanged += _makeReservationViewModel_PropertyChanged;
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return !string.IsNullOrEmpty(_makeReservationViewModel.Username) &&
+                _makeReservationViewModel.FloorNumber >= 0 &&
+                _makeReservationViewModel.RoomNumber > 0 &&
+                base.CanExecute(parameter);
+        }
+
+        public override void Execute(object? parameter)
+        {
+            Reservation reservation = new Reservation(
+                new RoomID(_makeReservationViewModel.FloorNumber, _makeReservationViewModel.RoomNumber),
+                _makeReservationViewModel.Username,
+                _makeReservationViewModel.StartDate,
+                _makeReservationViewModel.EndDate);
+
+            try
+            {
+                _hotel.MakeReservation(reservation);
+                MessageBox.Show("Successfully booked room.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+            catch (ReservationConflictException ex)
+            {
+                MessageBox.Show("This room is already booked.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private void _makeReservationViewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(MakeReservationViewModel.Username) || 
+                e.PropertyName == nameof(MakeReservationViewModel.FloorNumber) ||
+                e.PropertyName == nameof(MakeReservationViewModel.RoomNumber))
+                OnCanExecuteChanged();
+        }
+    }
+}

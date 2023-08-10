f5af4e9f9c6f98a230a86e52e1644e27bda5fdf8
Bartosz Kowalski <bezio1292@gmail.com>
2023-08-10 15:12:51 +0200

    Redefine constructor.

--- YouTubeViewers/ViewModels/YouTubeViewersListingItemViewModel.cs
+++ YouTubeViewers/ViewModels/YouTubeViewersListingItemViewModel.cs
@@ -4,18 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using YouTubeViewers.Models;
 
 namespace YouTubeViewers.ViewModels
 {
     class YouTubeViewersListingItemViewModel
     {
-        public string Username { get; }
+        public YouTubeViewer YouTubeViewer { get; }
+
+        public string Username => YouTubeViewer.Username;
         public ICommand EditCommand { get; }
         public ICommand DeleteCommand { get; }
 
-        public YouTubeViewersListingItemViewModel(string username)
+
+        public YouTubeViewersListingItemViewModel(YouTubeViewer youTubeViewer)
         {
-            Username = username;
+            YouTubeViewer = youTubeViewer;
         }
     }
 }

cb88af79684d703e1df7768ae9f3948db4f97298
Bartosz Kowalski <bezio1292@gmail.com>
2023-08-10 15:19:16 +0200

    Add store. Add event handler.

--- YouTubeViewers/ViewModels/YouTubeViewersDetailsViewModel.cs
+++ YouTubeViewers/ViewModels/YouTubeViewersDetailsViewModel.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YouTubeViewers.Models;
+using YouTubeViewers.Stores;
 
 namespace YouTubeViewers.ViewModels
 {
     class YouTubeViewersDetailsViewModel : ViewModelBase
     {
-        public string Username { get; }
-        public string IsSubscribedDisplay { get; }
-        public bool IsMember { get; }
+        private readonly SelectedYouTubeViewerStore _selectedYouTubeViewerStore;
 
-        public YouTubeViewersDetailsViewModel() 
+        private YouTubeViewer SelectedYouTubeViewer => _selectedYouTubeViewerStore.SelectedYouTubeViewer;
+
+        public bool HasSelectedYouTubeViewer => SelectedYouTubeViewer != null;
+        public string Username => SelectedYouTubeViewer?.Username ?? "Unknown";
+        public string IsSubscribedDisplay => (SelectedYouTubeViewer?.IsSubscribed ?? false) ? "Yes" : "No";
+        public bool IsMember => SelectedYouTubeViewer?.IsMember ?? false;
+
+        public YouTubeViewersDetailsViewModel(SelectedYouTubeViewerStore selectedYouTubeViewerStore) 
         {
-            Username = "Arata";
-            IsSubscribedDisplay = "Yes";
-            IsMember = true;
+            _selectedYouTubeViewerStore = selectedYouTubeViewerStore;
+            _selectedYouTubeViewerStore.SelectedYouTubeViewerChanged += _selectedYouTubeViewerStore_SelectedYouTubeViewerChanged;
+        }
+
+        private void _selectedYouTubeViewerStore_SelectedYouTubeViewerChanged()
+        {
+            OnPropertyChanged(nameof(HasSelectedYouTubeViewer));
+            OnPropertyChanged(nameof(Username));
+            OnPropertyChanged(nameof(IsSubscribedDisplay));
+            OnPropertyChanged(nameof(IsMember));
+        }
+
+        protected override void Dispose()
+        {
+            _selectedYouTubeViewerStore.SelectedYouTubeViewerChanged -= _selectedYouTubeViewerStore_SelectedYouTubeViewerChanged;
+            base.Dispose();
         }
     }
 }

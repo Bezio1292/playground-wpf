bc8788966085df03b6b847fd24e760e3d7d75b3c
Bartosz Kowalski <bezio1292@gmail.com>
2023-08-10 15:11:41 +0200

    Add store.

---
+++ YouTubeViewers/Stores/SelectedYouTubeViewerStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YouTubeViewers.Models;
+
+namespace YouTubeViewers.Stores
+{
+    class SelectedYouTubeViewerStore
+    {
+        private YouTubeViewer _selectedYouTubeViewer;
+        public YouTubeViewer SelectedYouTubeViewer
+        {
+            get { return _selectedYouTubeViewer; }
+            set
+            {
+                _selectedYouTubeViewer = value;
+                SelectedYouTubeViewerChanged?.Invoke();
+            }
+        }
+
+        public event Action SelectedYouTubeViewerChanged;
+    }
+}

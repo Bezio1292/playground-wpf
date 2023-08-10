19e6cde563f72d6751badd4f1f6a6825678afc76
Bartosz Kowalski <bezio1292@gmail.com>
2023-08-10 14:06:26 +0200

    Add view model.

---
+++ YouTubeViewers/ViewModels/YouTubeViewersDetailsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeViewers.ViewModels
+{
+    class YouTubeViewersDetailsViewModel : ViewModelBase
+    {
+        public string Username { get; }
+        public string IsSubscribedDisplay { get; }
+        public bool IsMember { get; }
+
+        public YouTubeViewersDetailsViewModel() 
+        {
+            Username = "Arata";
+            IsSubscribedDisplay = "Yes";
+            IsMember = true;
+        }
+    }
+}
